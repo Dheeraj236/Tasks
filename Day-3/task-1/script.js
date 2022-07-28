511d6288c018d0041a6f26fb016c1530f5d841ea
Dheeraj236 <100273040+Dheeraj236@users.noreply.github.com>
2022-07-28 18:50:32 +0530

    Add files via upload

---
+++ Day-3/task-1/script.js
@@ -0,0 +1,41 @@
+let Resume={
+    "basics": {
+      "name": "Dheeraj Kumar.P",
+      "email": "Dheeraj@gamil.com",
+      "phone": 9182771741,
+      "degree": "B.Tech",
+      "location": {
+        "address": "13/2/245, Ramachandra Nagar",
+        "postalCode": 515001,
+        "city": "Anantapur",
+        "state": "Andhra Pradesh",
+        "country": "India"
+      },
+    },
+    "education": [
+      {
+        "institution": "Anantha Lakshmi Engineering College",
+        "department": "Civil Engineering",
+        "batch start year": 2017,
+        "batch end year": 2021,
+        "gpa": 7.3,
+      }
+    ],
+    "skills": [
+      {
+        "name": "Javascript",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Telugu,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Gaming, Trading",
+      }
+    ]
+  }
+  console.log(Resume);
